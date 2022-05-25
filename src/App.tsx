706f3e13a5e21560ce0b455a838be29778783282
granddevv <spacingflight@outlook.com>
2022-05-25 17:29:02 +0700

    add resource

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { AppStateProvider } from "./contexts/app-state";
+import Layout from "./layout";
+
+function App() {
+  return (
+    <AppStateProvider>
+      <Layout />
+    </AppStateProvider>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "dark-gray": "#272d3b",
+        "white-text": "#fdfdfb",
+      },
+    },
+  },
+  plugins: [],
+};
